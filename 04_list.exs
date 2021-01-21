7b4f6727f763cf242d29425611e59cebc610552a
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-22 01:52:29 +0700

    Add list comprehension

--- 04_list.exs
+++ 04_list.exs
@@ -34,7 +34,25 @@ defmodule ListTest do
 
   test "List fordr" do
     list = [20, 10, 5, 2.5]
+    sum = List.foldr list, 0, fn (num, sum) -> num + sum end
+    assert sum == 37.5
+  end
+
+  test "Enum.reduce" do
+    list = [20, 10, 5, 2.5]
     sum = Enum.reduce list, 0, fn (num, sum) -> num + sum end
     assert sum == 37.5
+  end
+
+  test "wrap" do
+    assert List.wrap(sample()) == sample()
+    assert List.wrap(1) == [1]
+    assert List.wrap([]) == []
+    assert List.wrap(nil) == []
+  end
+
+  test "list comprehension" do
+    some = for n <- sample(), String.first(n) < "M", do: n <> " Morgan"
+    assert some == ["Jen Morgan", "Kai Morgan"]
   end
 end
