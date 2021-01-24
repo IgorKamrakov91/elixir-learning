c186099c503aef307c874812050b338afb0d5fca
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-25 03:45:26 +0700

    Add fibonacci test

--- 07_fibonacci.exs
+++ 07_fibonacci.exs
@@ -15,3 +15,18 @@ defmodule Fib do
     fib(acc ++ [Enum.at(acc, -2) + Enum.at(acc, -1)], n - 1)
   end
 end
+
+ExUnit.start()
+
+defmodule RecursionTest do
+  use ExUnit.Case
+
+  import Fib
+
+  test "fibonacci" do
+    assert fib(0) == []
+    assert fib(1) == [0]
+    assert fib(2) == [0, 1]
+    assert fib(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  end
+end
