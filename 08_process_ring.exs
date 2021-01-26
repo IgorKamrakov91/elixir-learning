a686de4cfd3dc2d445b1dc6cd990683abbd8a555
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-26 22:03:53 +0700

    Add sending another message to process

--- 08_process_ring.exs
+++ 08_process_ring.exs
@@ -2,6 +2,16 @@ defmodule Pinger do
   @doc "Send message (echo) to the next process in the ring."
   def ping(echo, limit) do
     receive do
+      # got message, send another message to the next process in the ring
+      {[next | rest], msg, count} when count <= limit ->
+        IO.puts("Received: #{inspect(msg)} (count #{count})")
+        :timer.sleep(1000)
+        send(next, {rest ++ [next], echo, count + 1})
+        ping(echo, limit)
+
+      # over our limit of messages, send :ok to ring
+      {[next | rest], _, _} ->
+        send(next, {rest, :ok})
 
       # someone told us to stop, so pass along the message
       {[next | rest], :ok} ->
