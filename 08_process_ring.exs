6e61393941adceb6da28c88dba1fd12196bd00de
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-26 21:58:37 +0700

    Add stop receiving to ping

--- 08_process_ring.exs
+++ 08_process_ring.exs
@@ -1,9 +1,14 @@
 defmodule Pinger do
-  def ping do
-    
+  @doc "Send message (echo) to the next process in the ring."
+  def ping(echo, limit) do
+    receive do
+
+      # someone told us to stop, so pass along the message
+      {[next | rest], :ok} ->
+        send(next, {rest, :ok})
+    end
   end
 end
 
 defmodule Spawner do
-
 end
