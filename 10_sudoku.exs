ad124611bf88c50565b9d2da7b47dd560dcdbf41
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-28 00:05:20 +0700

    Add cols solved function

--- 10_sudoku.exs
+++ 10_sudoku.exs
@@ -6,11 +6,21 @@ defmodule Sudoku do
   end
 
   def rows_solved?([]), do: true
+
   def rows_solved?([row | rest]) do
     max = count(row)
     sort(row) == to_list(1..max) and rows_solved?(rest)
   end
 
   def cols_solved?(board) do
+    do_cols_solved?(board, count(board) - 1)
   end
+
+  defp do_cols_solved?(board, index) when index >= 0 do
+    max = count(board)
+    col = map(board, fn row -> at(row, index) end)
+    sort(col) == to_list(1..max) and do_cols_solved?(board, index - 1)
+  end
+
+  defp do_cols_solved?(_, -1), do: true
 end
