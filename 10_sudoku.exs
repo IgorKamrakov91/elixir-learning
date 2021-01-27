aa470043819d0a10753c4ad9fc015d644ff06dc9
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-27 23:58:44 +0700

    Add sudoku module

---
+++ 10_sudoku.exs
@@ -0,0 +1,13 @@
+defmodule Sudoku do
+  import Enum
+
+  def solved?(board) do
+    rows_solved?(board) and cols_solved?(board)
+  end
+
+  def rows_solved?(board) do
+  end
+
+  def cols_solved?(board) do
+  end
+end
