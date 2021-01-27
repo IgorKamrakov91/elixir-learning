a78bf43713d0ad3bdba909eb527ff770b4441528
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-28 00:01:01 +0700

    Add rows_solved fun

--- 10_sudoku.exs
+++ 10_sudoku.exs
@@ -5,7 +5,10 @@ defmodule Sudoku do
     rows_solved?(board) and cols_solved?(board)
   end
 
-  def rows_solved?(board) do
+  def rows_solved?([]), do: true
+  def rows_solved?([row | rest]) do
+    max = count(row)
+    sort(row) == to_list(1..max) and rows_solved?(rest)
   end
 
   def cols_solved?(board) do
