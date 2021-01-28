73984625ba64d8d0fdad8de4d6eee18e221628ed
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-29 02:44:13 +0700

    Add empty tests

---
+++ 11_sudoku_solver.exs
@@ -0,0 +1,15 @@
+ExUnit.start()
+
+defmodule SudokuSolverTest do
+  use ExUnit.Case
+  import SudokuSolver
+
+  test "correct behavior of combinations" do
+  end
+
+  test "solves small board" do
+  end
+
+  test "return nil on unsolvable board" do
+  end
+end
