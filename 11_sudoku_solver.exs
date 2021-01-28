81c21fd1fbba696af61d18d0a5bac50a11c5bfbf
Igor Kamrakov <i.kamrakov@gmail.com>
2021-01-29 03:06:17 +0700

    Add combinations tests and function

--- 11_sudoku_solver.exs
+++ 11_sudoku_solver.exs
@@ -1,3 +1,27 @@
+defmodule SudokuSolver do
+  @doc """
+  Given a list of possibilities for each row, return all possible combinations.
+  """
+  def combinations([]), do: [[]]
+  def combinations([list | rest]) do
+    crest = combinations(rest)
+    for perm <- permutations(list), res <- crest do
+      List.flatten([perm | res])
+    end
+  end
+
+  @doc """
+  Return all possible permuations of a list.
+  ## Example
+    iex> SudokuSolver.permutations([1,2,3])
+    [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+  """
+  def permutations([]), do: []
+  def permutations(list) do
+    for head <- list, tail <- permutations(list -- [head]), do: [head | tail]
+  end
+end
+
 ExUnit.start()
 
 defmodule SudokuSolverTest do
@@ -5,6 +29,8 @@ defmodule SudokuSolverTest do
   import SudokuSolver
 
   test "correct behavior of combinations" do
+    assert combinations([[2, 3], [1]]) == [[2, 3, 1], [3, 2, 1]]
+    assert combinations([[1], [2], [3, 4]]) == [[1, 2, 3, 4], [1, 2, 4, 3]]
   end
 
   test "solves small board" do
