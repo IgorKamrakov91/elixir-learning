79562d3fc1e4fb5c5a440e905dc0e76c3f7eab70
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-02 22:03:21 +0700

    Add shuffle function

--- 13_card_deck.exs
+++ 13_card_deck.exs
@@ -7,4 +7,18 @@ defmodule Deck do
         face <- [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
         do: {suit, face}
   end
+
+  # Given a list of cards (a deck), reorder cards randomly.
+  # If no deck is given, then create a new one and shuffle that.
+  def shuffle(deck \\ new()) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Given a deck of cards, a list of players, and a deal function,
+    call the deal function for each card for each player. The function
+    should return the updated player.
+    Returns the list of players.
+  """
+  def _deal([], players, _, _), do: {players, []}
 end
