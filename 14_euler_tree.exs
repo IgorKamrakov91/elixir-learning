ed31d555c62a9a232354c18767473e78befb1ab4
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-04 02:46:27 +0700

    Add Tree module

---
+++ 14_euler_tree.exs
@@ -0,0 +1,14 @@
+defmodule Tree do
+  @moduledoc """
+  Project Euler - Problem 67
+  By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+     3
+    7 4
+   2 4 6
+  8 5 9 3
+  That is, 3 + 7 + 4 + 9 = 23.
+  Find the maximum total from top to bottom in tree.txt, a 15K text file containing a triangle with one-hundred rows.
+  """
+
+  
+end
