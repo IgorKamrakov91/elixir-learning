9293ce535574aca9ebfc9598de6b33186f224ebf
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-05 01:56:28 +0700

    Add eat function

--- 15_eating_philosophers.exs
+++ 15_eating_philosophers.exs
@@ -33,19 +33,27 @@ defmodule Table do
     # distribute forks to waiting philosophers
     if length(waiting) > 0 do
       names = for {_, phil} <- waiting, do: phil.name
-      IO.puts "#{length waiting} philosopher#{if length(waiting) == 1, do: '', else: 's'} waiting: #{Enum.join names, ", "}"
+
+      IO.puts(
+        "#{length(waiting)} philosopher#{if length(waiting) == 1, do: '', else: 's'} waiting: #{
+          Enum.join(names, ", ")
+        }"
+      )
+
       if length(forks) >= 2 do
         [{pid, _}, waiting] = waiting
         [fork1, fork2 | forks] = forks
-        send pid, {:eat, [fork1, fork2]}
+        send(pid, {:eat, [fork1, fork2]})
       end
     end
+
     receive do
       {:sit_down, pid, phil} ->
         manage_resources(forks, [{pid, phil} | waiting])
+
       {:give_up_seat, free_forks, _} ->
         forks = free_forks ++ forks
-        IO.puts "#{length forks} fork#{if length(forks) == 1, do: '', else: 's'} available"
+        IO.puts("#{length(forks)} fork#{if length(forks) == 1, do: '', else: 's'} available")
         manage_resources(forks, waiting)
     end
   end
@@ -53,21 +61,28 @@ end
 
 defmodule Dine do
   def dine(phil, table) do
-    send table, {:sit_table, self, phil}
+    send(table, {:sit_table, self, phil})
+
     receive do
       {:eat, forks} ->
         phil = eat(phil, forks, table)
         phil = think(phil, table)
     end
+
     dine(phil, table)
   end
 
   def eat(phil, forks, table) do
-
+    phil = %{phil | ate: phil.ate + 1}
+    IO.puts("#{phil.name} is eating (count: #{phil.ate})")
+    :timer.sleep(:random.uniform(1000))
+    IO.puts("#{phil.name} is done eating")
+    send(table, {:give_up_seat, forks, phil})
+    phil
   end
 
   def think(phil, _) do
-    IO.puts "#{phil.name} is thinking (count: #{phil.thunk})"
+    IO.puts("#{phil.name} is thinking (count: #{phil.thunk})")
     :timer.sleep(:random.uniform(1000))
     %{phil | thunk: phil.thunk + 1}
   end
