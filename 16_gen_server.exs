e188e42505a76f4d74ffeeb2a7f325bab63441b6
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-05 19:22:31 +0000

    Trim whitespaces

--- 16_gen_server.exs
+++ 16_gen_server.exs
@@ -6,7 +6,7 @@ defmodule	PrimeFactors do
   @doc """
   Given a number, return a list of prime factors.
   """
-	
+
 	def prime_factors(number, div \\ 2, factors \\ []) do
 		cond do
 			prime?(number) ->
@@ -14,15 +14,15 @@ defmodule	PrimeFactors do
 			rem(number, div) == 0 ->
 				prime_factors(div(number, div), 2, [div | factors])
 			true ->
-				prime_factors(number, div + 1, factors)	
+				prime_factors(number, div + 1, factors)
 		end
 	end
-		
+
 	def prime?(2), do: true
 	def prime?(number) do
 		prime?(number, :math.sqrt(number) |> :erlang.trunc)
 	end
-	
+
 	defp prime?(_, 1), do: true
 	defp prime?(number, div) do
 		if rem(number, div) == 0 do
@@ -37,27 +37,27 @@ defmodule PrimeFactorsServer do
   @moduledoc """
   Wrap the PrimeFactors.prime_factors functionality in a server.
   """
-	
+
 	use GenServer
 	import PrimeFactors
-	
+
 	def start_link do
 		:gen_server.start_link({:local, :prime_factors}, __MODULE__, [], [])
 	end
-	
+
 	def init(_args) do
 		{:ok, []}
 	end
-	
+
 	# synchronous
 	def handle_call(:flush, _from, context) do
 		{:reply, context, []}
 	end
-	
+
 	def handle_call(num, _from, context) do
 		{:reply, prime_factors(num), context}
 	end
-	
+
 	# asynchronous
 	def handle_cast(num, context) do
 		{:noreply, [{num, prime_factors(num)} | context]}
@@ -69,18 +69,18 @@ ExUnit.start
 defmodule PrimeFactorsTest do
 	use ExUnit.Case
 	import PrimeFactors
-	
+
 	test "prime_factors" do
 		assert prime_factors(10) == [5, 2]
 		assert prime_factors(100) == [5, 5, 2, 2]
 	end
-	
+
 	test "prime?" do
 		assert(prime?(2))
 	  assert(prime?(11))
 	  assert(not prime?(8))
 	end
-	
+
 	test "async" do
 	  {:ok, pid} = PrimeFactorsServer.start_link
 		:gen_server.cast(pid, 10)
