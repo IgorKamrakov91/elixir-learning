1bdd6b9c12e77ecd068cd2f560b777647682df75
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-07 10:09:24 +0000

    Add run function

--- 18_inets.exs
+++ 18_inets.exs
@@ -1,6 +1,14 @@
 require Record
 
 defmodule WebServer do
+	def run do
+		:inets.start()
+		options = [server_name: 'foo', server_root: '/tmp', document_root: '/tmp', port: 3000, modules: [App]]
+		{:ok, _pid} = :inets.start(:httpd, options)
+		IO.puts("running on port 3000")
+		receive do: (_ -> :ok)
+
+	end
 end
 
 defmodule App do
