d407542b84a83f60095666185fd601f9323494da
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-07 11:01:13 +0000

    Add wiki module and doc

---
+++ 19_wiki.exs
@@ -0,0 +1,15 @@
+require IEx
+require Record
+
+defmodule Wiki do
+	@moduledoc """
+	A very simple wiki.
+	CamelCase words are auto-linked.
+	Pages are stored in the support/wiki directory.
+	To run:
+	$ iex 21-wiki.exs
+	... then point your browser to http://localhost:3000
+"""
+
+
+end
