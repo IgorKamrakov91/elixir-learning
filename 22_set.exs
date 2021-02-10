7f720a566634251263235b206c01d97c476526e1
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-11 05:00:38 +0700

    Change to HashSet

--- 22_set.exs
+++ 22_set.exs
@@ -4,12 +4,16 @@ defmodule SetTest do
   use ExUnit.Case
 
   test "to_list" do
-    list = Enum.into([1, 2, 2, 3], MapSet.new()) |> MapSet.to_list()
+    list = Enum.into([1, 2, 2, 3], HashSet.new()) |> HashSet.to_list()
+    # unintuitive ordering
     assert list == [2, 3, 1]
   end
 
   test "union" do
-    union = Enum.into([1, 2, 3], MapSet.new()) |> MapSet.union(Enum.into([2, 3, 4], MapSet.new()))
-    assert MapSet.to_list(union) == [2, 3, 4, 1]
+    union =
+      Enum.into([1, 2, 3], HashSet.new()) |> HashSet.union(Enum.into([2, 3, 4], HashSet.new()))
+
+    # unintuitive, but somewhat more understandable
+    assert Set.to_list(union) == [2, 3, 4, 1]
   end
 end
