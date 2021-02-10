2337533001e5d98e5c047264748aafcbf18584ff
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-11 04:58:40 +0700

    Add union test

--- 22_set.exs
+++ 22_set.exs
@@ -7,4 +7,9 @@ defmodule SetTest do
     list = Enum.into([1, 2, 2, 3], MapSet.new()) |> MapSet.to_list()
     assert list == [2, 3, 1]
   end
+
+  test "union" do
+    union = Enum.into([1, 2, 3], MapSet.new()) |> MapSet.union(Enum.into([2, 3, 4], MapSet.new()))
+    assert MapSet.to_list(union) == [2, 3, 4, 1]
+  end
 end
