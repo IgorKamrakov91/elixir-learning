628e401c8ab8c7cfab7da0730d00e7f98497d715
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-11 04:55:57 +0700

    Add to list test

--- 22_set.exs
+++ 22_set.exs
@@ -1,5 +1,10 @@
 ExUnit.start()
 
 defmodule SetTest do
+  use ExUnit.Case
 
+  test "to_list" do
+    list = Enum.into([1, 2, 2, 3], MapSet.new()) |> MapSet.to_list()
+    assert list == [2, 3, 1]
+  end
 end
