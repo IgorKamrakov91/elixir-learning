751056bea631c76f9e0944c46d0019f5ff3363f7
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-11 05:02:57 +0700

    Add intersection test

--- 22_set.exs
+++ 22_set.exs
@@ -18,6 +18,11 @@ defmodule SetTest do
   end
 
   test "intersection" do
+    union =
+      Enum.into([1, 2, 3], HashSet.new())
+      |> HashSet.intersection(Enum.into([2, 3, 4], HashSet.new()))
+
+    assert Set.to_list(union) == [2, 3]
   end
 
   test "member?" do
