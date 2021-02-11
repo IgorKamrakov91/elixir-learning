92370188fdb09dc260a9bcc02954e252d7ada501
Igor Kamrakov <i.kamrakov@gmail.com>
2021-02-12 04:15:42 +0700

    Change add entry function

--- 23_todolist.exs
+++ 23_todolist.exs
@@ -3,8 +3,11 @@
 defmodule TodoList do
   def new(), do: MultiList.new()
 
-  def add_entry(todo_list, date, title) do
-    MultiList.add(todo_list, date, title)
+  # example how to create entry
+  # entry = %{date: ~D[2018-12-19], title: "Dentist"}
+  # and add it with add_entry function
+  def add_entry(todo_list, entry) do
+    MultiList.add(todo_list, entry.date, entry.title)
   end
 
   def entries(todo_list, date) do
